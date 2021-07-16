0121ab4e021e30a6394c4c96ef728bca67bd4b03
ghramcode18 <81458028+ghramcode18@users.noreply.github.com>
2021-07-16 20:40:23 +0300

    --

--- src/main/java/org/closure/gcp/entities/UserEntity.java
+++ src/main/java/org/closure/gcp/entities/UserEntity.java
@@ -1,34 +1,26 @@
 package org.closure.gcp.entities;
 
-import java.util.Date;
-import java.util.List;
-import java.util.Objects;
-
+ import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import org.closure.gcp.models.Gender;
 import org.springframework.format.annotation.DateTimeFormat;
 
-
-
 @Entity
 @Table(name = "users")
 public class UserEntity {
-    @Id
+    @Id 
     @GeneratedValue(strategy = GenerationType.TABLE)
-    private Integer id;
+    private Integer Id;
 
     @Column(nullable = true)
-    private String name;
+        private String Username;
 
     @Column(nullable = true)
     private String email;
@@ -41,289 +33,344 @@ public class UserEntity {
 
     @Column(nullable = true)
     private String address; 
-  
+
     @Column(nullable = true)
     private Gender gender;
-    
+
     @Column(nullable = true)
     private String image;
-    
+
     @ManyToOne(optional = true)
     private CollegeEntity college;
     
-    @OneToMany(mappedBy = "user")
-    private List<InterestEntity> interests;
+        
+ }
+// package org.closure.gcp.entities;
+
+// import java.util.Date;
+// import java.util.List;
+// import java.util.Objects;
+
+// import javax.persistence.Column;
+// import javax.persistence.Entity;
+// import javax.persistence.GeneratedValue;
+// import javax.persistence.GenerationType;
+// import javax.persistence.Id;
+// import javax.persistence.ManyToMany;
+// import javax.persistence.ManyToOne;
+// import javax.persistence.OneToMany;
+// import javax.persistence.OneToOne;
+// import javax.persistence.Table;
+
+// import org.closure.gcp.models.Gender;
+// import org.springframework.format.annotation.DateTimeFormat;
+
+
+
+// @Entity
+// @Table(name = "users")
+// public class UserEntity {
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.TABLE)
+//     private Integer id;
+
+//     @Column(nullable = true)
+//     private String name;
+
+//     @Column(nullable = true)
+//     private String email;
+
+//     @Column(nullable = true)
+//     private String password;
+
+//     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+//     private Date birthday;
+
+//     @Column(nullable = true)
+//     private String address; 
+  
+//     @Column(nullable = true)
+//     private Gender gender;
     
-    @OneToOne
-    private ScoreEntity score;
+//     @Column(nullable = true)
+//     private String image;
+    
+//     @ManyToOne(optional = true)
+//     private CollegeEntity college;
+    
+//     @OneToMany(mappedBy = "user")
+//     private List<InterestEntity> interests;
+    
+//     @OneToOne
+//     private ScoreEntity score;
 
    
-    @ManyToOne(optional = true)
-    private LevelEntity level;
+//     @ManyToOne(optional = true)
+//     private LevelEntity level;
 
-    @ManyToMany
-    private List<ContestEntity> contests;
+//     @ManyToMany
+//     private List<ContestEntity> contests;
 
-    @OneToMany(mappedBy = "user")
-    private List<RankEntity> rank;
-
-
-    @OneToMany(mappedBy = "user")
-    private List<FireBaseEntity> firebase;
+//     @OneToMany(mappedBy = "user")
+//     private List<RankEntity> rank;
 
 
-    public UserEntity() {
-    }
+//     @OneToMany(mappedBy = "user")
+//     private List<FireBaseEntity> firebase;
 
-    public UserEntity(Integer id, String name, String email, String password, Date birthday, String address, CollegeEntity college, Gender gender, String image, List<InterestEntity> interests, ScoreEntity score, LevelEntity level, List<ContestEntity> contests, List<RankEntity> rank, List<FireBaseEntity> firebase) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.password = password;
-        this.birthday = birthday;
-        this.address = address;
-        this.college = college;
-        this.gender = gender;
-        this.image = image;
-        this.interests = interests;
-        this.score = score;
-        this.level = level;
-        this.contests = contests;
-        this.rank = rank;
-        this.firebase = firebase;
-    }
 
-    public Integer getId() {
-        return this.id;
-    }
+//     public UserEntity() {
+//     }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+//     public UserEntity(Integer id, String name, String email, String password, Date birthday, String address, CollegeEntity college, Gender gender, String image, List<InterestEntity> interests, ScoreEntity score, LevelEntity level, List<ContestEntity> contests, List<RankEntity> rank, List<FireBaseEntity> firebase) {
+//         this.id = id;
+//         this.name = name;
+//         this.email = email;
+//         this.password = password;
+//         this.birthday = birthday;
+//         this.address = address;
+//         this.college = college;
+//         this.gender = gender;
+//         this.image = image;
+//         this.interests = interests;
+//         this.score = score;
+//         this.level = level;
+//         this.contests = contests;
+//         this.rank = rank;
+//         this.firebase = firebase;
+//     }
 
-    public String getName() {
-        return this.name;
-    }
+//     public Integer getId() {
+//         return this.id;
+//     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+//     public void setId(Integer id) {
+//         this.id = id;
+//     }
 
-    public String getEmail() {
-        return this.email;
-    }
+//     public String getName() {
+//         return this.name;
+//     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+//     public void setName(String name) {
+//         this.name = name;
+//     }
 
-    public String getPassword() {
-        return this.password;
-    }
+//     public String getEmail() {
+//         return this.email;
+//     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+//     public void setEmail(String email) {
+//         this.email = email;
+//     }
 
-    public Date getBirthday() {
-        return this.birthday;
-    }
+//     public String getPassword() {
+//         return this.password;
+//     }
 
-    public void setBirthday(Date birthday) {
-        this.birthday = birthday;
-    }
+//     public void setPassword(String password) {
+//         this.password = password;
+//     }
 
-    public String getAddress() {
-        return this.address;
-    }
+//     public Date getBirthday() {
+//         return this.birthday;
+//     }
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
+//     public void setBirthday(Date birthday) {
+//         this.birthday = birthday;
+//     }
 
-    public CollegeEntity getCollege() {
-        return this.college;
-    }
+//     public String getAddress() {
+//         return this.address;
+//     }
 
-    public void setCollege(CollegeEntity college) {
-        this.college = college;
-    }
+//     public void setAddress(String address) {
+//         this.address = address;
+//     }
 
-    public Gender getGender() {
-        return this.gender;
-    }
+//     public CollegeEntity getCollege() {
+//         return this.college;
+//     }
 
-    public void setGender(Gender gender) {
-        this.gender = gender;
-    }
+//     public void setCollege(CollegeEntity college) {
+//         this.college = college;
+//     }
 
-    public String getImage() {
-        return this.image;
-    }
+//     public Gender getGender() {
+//         return this.gender;
+//     }
 
-    public void setImage(String image) {
-        this.image = image;
-    }
+//     public void setGender(Gender gender) {
+//         this.gender = gender;
+//     }
 
-    public List<InterestEntity> getInterests() {
-        return this.interests;
-    }
+//     public String getImage() {
+//         return this.image;
+//     }
 
-    public void setInterests(List<InterestEntity> interests) {
-        this.interests = interests;
-    }
+//     public void setImage(String image) {
+//         this.image = image;
+//     }
 
-    public ScoreEntity getScore() {
-        return this.score;
-    }
+//     public List<InterestEntity> getInterests() {
+//         return this.interests;
+//     }
 
-    public void setScore(ScoreEntity score) {
-        this.score = score;
-    }
+//     public void setInterests(List<InterestEntity> interests) {
+//         this.interests = interests;
+//     }
 
-    public LevelEntity getLevel() {
-        return this.level;
-    }
+//     public ScoreEntity getScore() {
+//         return this.score;
+//     }
 
-    public void setLevel(LevelEntity level) {
-        this.level = level;
-    }
+//     public void setScore(ScoreEntity score) {
+//         this.score = score;
+//     }
 
-    public List<ContestEntity> getContests() {
-        return this.contests;
-    }
+//     public LevelEntity getLevel() {
+//         return this.level;
+//     }
 
-    public void setContests(List<ContestEntity> contests) {
-        this.contests = contests;
-    }
+//     public void setLevel(LevelEntity level) {
+//         this.level = level;
+//     }
 
-    public List<RankEntity> getRank() {
-        return this.rank;
-    }
+//     public List<ContestEntity> getContests() {
+//         return this.contests;
+//     }
 
-    public void setRank(List<RankEntity> rank) {
-        this.rank = rank;
-    }
+//     public void setContests(List<ContestEntity> contests) {
+//         this.contests = contests;
+//     }
 
-    public List<FireBaseEntity> getFirebase() {
-        return this.firebase;
-    }
+//     public List<RankEntity> getRank() {
+//         return this.rank;
+//     }
 
-    public void setFirebase(List<FireBaseEntity> firebase) {
-        this.firebase = firebase;
-    }
+//     public void setRank(List<RankEntity> rank) {
+//         this.rank = rank;
+//     }
 
-    public UserEntity id(Integer id) {
-        setId(id);
-        return this;
-    }
+//     public List<FireBaseEntity> getFirebase() {
+//         return this.firebase;
+//     }
 
-    public UserEntity name(String name) {
-        setName(name);
-        return this;
-    }
+//     public void setFirebase(List<FireBaseEntity> firebase) {
+//         this.firebase = firebase;
+//     }
 
-    public UserEntity email(String email) {
-        setEmail(email);
-        return this;
-    }
+//     public UserEntity id(Integer id) {
+//         setId(id);
+//         return this;
+//     }
 
-    public UserEntity password(String password) {
-        setPassword(password);
-        return this;
-    }
+//     public UserEntity name(String name) {
+//         setName(name);
+//         return this;
+//     }
 
-    public UserEntity birthday(Date birthday) {
-        setBirthday(birthday);
-        return this;
-    }
+//     public UserEntity email(String email) {
+//         setEmail(email);
+//         return this;
+//     }
 
-    public UserEntity address(String address) {
-        setAddress(address);
-        return this;
-    }
+//     public UserEntity password(String password) {
+//         setPassword(password);
+//         return this;
+//     }
 
-    public UserEntity college(CollegeEntity college) {
-        setCollege(college);
-        return this;
-    }
+//     public UserEntity birthday(Date birthday) {
+//         setBirthday(birthday);
+//         return this;
+//     }
 
-    public UserEntity gender(Gender gender) {
-        setGender(gender);
-        return this;
-    }
+//     public UserEntity address(String address) {
+//         setAddress(address);
+//         return this;
+//     }
 
-    public UserEntity image(String image) {
-        setImage(image);
-        return this;
-    }
+//     public UserEntity college(CollegeEntity college) {
+//         setCollege(college);
+//         return this;
+//     }
 
-    public UserEntity interests(List<InterestEntity> interests) {
-        setInterests(interests);
-        return this;
-    }
+//     public UserEntity gender(Gender gender) {
+//         setGender(gender);
+//         return this;
+//     }
 
-    public UserEntity score(ScoreEntity score) {
-        setScore(score);
-        return this;
-    }
+//     public UserEntity image(String image) {
+//         setImage(image);
+//         return this;
+//     }
 
-    public UserEntity level(LevelEntity level) {
-        setLevel(level);
-        return this;
-    }
+//     public UserEntity interests(List<InterestEntity> interests) {
+//         setInterests(interests);
+//         return this;
+//     }
 
-    public UserEntity contests(List<ContestEntity> contests) {
-        setContests(contests);
-        return this;
-    }
+//     public UserEntity score(ScoreEntity score) {
+//         setScore(score);
+//         return this;
+//     }
 
-    public UserEntity rank(List<RankEntity> rank) {
-        setRank(rank);
-        return this;
-    }
+//     public UserEntity level(LevelEntity level) {
+//         setLevel(level);
+//         return this;
+//     }
 
-    public UserEntity firebase(List<FireBaseEntity> firebase) {
-        setFirebase(firebase);
-        return this;
-    }
+//     public UserEntity contests(List<ContestEntity> contests) {
+//         setContests(contests);
+//         return this;
+//     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (o == this)
-            return true;
-        if (!(o instanceof UserEntity)) {
-            return false;
-        }
-        UserEntity userEntity = (UserEntity) o;
-        return Objects.equals(id, userEntity.id) && Objects.equals(name, userEntity.name) && Objects.equals(email, userEntity.email) && Objects.equals(password, userEntity.password) && Objects.equals(birthday, userEntity.birthday) && Objects.equals(address, userEntity.address) && Objects.equals(college, userEntity.college) && Objects.equals(gender, userEntity.gender) && Objects.equals(image, userEntity.image) && Objects.equals(interests, userEntity.interests) && Objects.equals(score, userEntity.score) && Objects.equals(level, userEntity.level) && Objects.equals(contests, userEntity.contests) && Objects.equals(rank, userEntity.rank) && Objects.equals(firebase, userEntity.firebase);
-    }
+//     public UserEntity rank(List<RankEntity> rank) {
+//         setRank(rank);
+//         return this;
+//     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, email, password, birthday, address, college, gender, image, interests, score, level, contests, rank, firebase);
-    }
+//     public UserEntity firebase(List<FireBaseEntity> firebase) {
+//         setFirebase(firebase);
+//         return this;
+//     }
 
-    @Override
-    public String toString() {
-        return "{" +
-            " id='" + getId() + "'" +
-            ", name='" + getName() + "'" +
-            ", email='" + getEmail() + "'" +
-            ", password='" + getPassword() + "'" +
-            ", birthday='" + getBirthday() + "'" +
-            ", address='" + getAddress() + "'" +
-            ", college='" + getCollege() + "'" +
-            ", gender='" + getGender() + "'" +
-            ", image='" + getImage() + "'" +
-            ", interests='" + getInterests() + "'" +
-            ", score='" + getScore() + "'" +
-            ", level='" + getLevel() + "'" +
-            ", contests='" + getContests() + "'" +
-            ", rank='" + getRank() + "'" +
-            ", firebase='" + getFirebase() + "'" +
-            "}";
-    }
+//     @Override
+//     public boolean equals(Object o) {
+//         if (o == this)
+//             return true;
+//         if (!(o instanceof UserEntity)) {
+//             return false;
+//         }
+//         UserEntity userEntity = (UserEntity) o;
+//         return Objects.equals(id, userEntity.id) && Objects.equals(name, userEntity.name) && Objects.equals(email, userEntity.email) && Objects.equals(password, userEntity.password) && Objects.equals(birthday, userEntity.birthday) && Objects.equals(address, userEntity.address) && Objects.equals(college, userEntity.college) && Objects.equals(gender, userEntity.gender) && Objects.equals(image, userEntity.image) && Objects.equals(interests, userEntity.interests) && Objects.equals(score, userEntity.score) && Objects.equals(level, userEntity.level) && Objects.equals(contests, userEntity.contests) && Objects.equals(rank, userEntity.rank) && Objects.equals(firebase, userEntity.firebase);
+//     }
+
+//     @Override
+//     public int hashCode() {
+//         return Objects.hash(id, name, email, password, birthday, address, college, gender, image, interests, score, level, contests, rank, firebase);
+//     }
+
+//     @Override
+//     public String toString() {
+//         return "{" +
+//             " id='" + getId() + "'" +
+//             ", name='" + getName() + "'" +
+//             ", email='" + getEmail() + "'" +
+//             ", password='" + getPassword() + "'" +
+//             ", birthday='" + getBirthday() + "'" +
+//             ", address='" + getAddress() + "'" +
+//             ", college='" + getCollege() + "'" +
+//             ", gender='" + getGender() + "'" +
+//             ", image='" + getImage() + "'" +
+//             ", interests='" + getInterests() + "'" +
+//             ", score='" + getScore() + "'" +
+//             ", level='" + getLevel() + "'" +
+//             ", contests='" + getContests() + "'" +
+//             ", rank='" + getRank() + "'" +
+//             ", firebase='" + getFirebase() + "'" +
+//             "}";
+//     }
 
 
 
@@ -332,4 +379,4 @@ public class UserEntity {
 
    
 
-}
+// }
